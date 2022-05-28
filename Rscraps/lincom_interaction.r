e1c33cf58caff14cdc5cad35863745c10fad4dd5
pithymaxim <maximplaysbass@gmail.com>
2022-05-28 09:29:11 -0700

    Update lincom_interaction.r

--- Rscraps/lincom_interaction.r
+++ Rscraps/lincom_interaction.r
@@ -1,4 +1,4 @@
-library(foreign)
+library(foreign) # To load Stata datasets
 library(multcomp) # for glht (https://www.rdocumentation.org/packages/multcomp/versions/1.4-19/topics/glht)
 library(estimatr) # for lm_robust
 
@@ -11,5 +11,11 @@ summary(model)
 # Show names
 names(coef(model))
 # Need backticks to have spaces in the coefficient names 
+
+# College premium for married people:
 summary(glht(model,
            linfct=c("`collgradcollege grad` + `marriedmarried:collgradcollege grad` = 0")))
+
+# Wage gain for being married and in college relative to a single non-college:
+summary(glht(model,
+             linfct=c("marriedmarried + `collgradcollege grad` + `marriedmarried:collgradcollege grad` = 0")))

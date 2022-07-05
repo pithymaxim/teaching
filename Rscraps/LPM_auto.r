67aaa89328218f666e580790d65368e566635f39
pithymaxim <maximplaysbass@gmail.com>
2022-07-04 20:41:27 -0700

    Update LPM_auto.r

--- Rscraps/LPM_auto.r
+++ Rscraps/LPM_auto.r
@@ -14,3 +14,13 @@ mymodel = lm(foreign.dummy ~ mpg + price, data=auto)
 
 # Make the SEs robust 
 coeftest(mymodel, vcov = vcovHC(mymodel, type="HC1"))
+
+# # Output
+# > coeftest(mymodel, vcov = vcovHC(mymodel, type="HC1"))
+
+# t test of coefficients:
+
+#                 Estimate   Std. Error t value    Pr(>|t|)    
+# (Intercept) -0.893459808  0.247015276 -3.6170    0.000554 ***
+# mpg          0.042422693  0.008512812  4.9834 0.000004247 ***
+# price        0.000046595  0.000021171  2.2009    0.030996 *  

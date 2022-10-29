56dbaa7212165e940967e4fd8a36347b172327b7
pithymaxim <maximplaysbass@gmail.com>
2022-10-29 05:29:11 -0700

    Update recid.r

--- Rscraps/survival/recid.r
+++ Rscraps/survival/recid.r
@@ -4,10 +4,12 @@ library(ggplot2)
 library(ggfortify)
 data("recid")
 recid$arrested = 1 - recid$cens
-recid$durat = recid$durat / 12
+
+# Make duration yearly?
+# recid$durat = recid$durat / 12 
 
 km <- with(recid, Surv(durat, arrested))
 
 km_fit <- survfit(Surv(durat, arrested) ~ 1, data=recid)
-summary(km_fit, times =seq(0,60,12) )
-autoplot(km_fit,)
+summary(km_fit)
+autoplot(km_fit)

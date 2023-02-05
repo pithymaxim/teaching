1ee755a7f5f428c0a2b5d9f507e147c7fec160a5
pithymaxim <maximplaysbass@gmail.com>
2023-02-04 22:10:02 -0800

    Update split_by_married.r

--- Rscraps/survival/split_by_married.r
+++ Rscraps/survival/split_by_married.r
@@ -17,6 +17,6 @@ km_split <- survfit(Surv(durat, arrested) ~ married, data=recid)
 
 # Made the months into yearly labels. See the arguments explained here: http://rpkgs.datanovia.com/survminer/reference/ggsurvplot_arguments.html
 ggsurvplot(km_split, xscale=12, break.x.by=12, xlim=c(0,7*12),
-          ylab="Share not yet arrested", xlab="Years")
+          ylab="Share not yet arrested", xlab="Years since release")
 
 

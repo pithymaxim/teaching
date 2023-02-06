9b35715884b17d02f62fc19069bbd00bba9423b1
pithymaxim <maximplaysbass@gmail.com>
2023-02-05 18:35:53 -0800

    Update recid.r

--- Rscraps/survival/recid.r
+++ Rscraps/survival/recid.r
@@ -12,3 +12,13 @@ km <- with(recid, Surv(durat, arrested))
 km_fit <- survfit(Surv(durat, arrested) ~ 1, data=recid)
 summary(km_fit)
 plot(km_fit)
+
+### Make the hazard. There is not an immediate way, none of these answers quite do it https://stackoverflow.com/a/67596112/1810279 ###
+# First need to convert KM results to dataframe 
+df = fortify(km_fit)
+# Then the hazard is = [surv(t-1) - surv(t)] / surv(t-1)
+# We can implement the lag with vector subsetting:
+hazard = ( df$surv[1:length(df$surv)-1] -  df$surv[2:length(df$surv)])/ df$surv[1:length(df$surv)-1]
+plot(df$time[-1],hazard, type="l",xlab="Time in months",ylab="Raw hazard")
+  
+

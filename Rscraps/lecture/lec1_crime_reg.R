2c759f704c1baa58c5fbf5a24f697defdbbd8849
pithymaxim <maximplaysbass@gmail.com>
2023-02-26 14:07:51 -0800

    Update lec1_crime_reg.R

--- Rscraps/lecture/lec1_crime_reg.R
+++ Rscraps/lecture/lec1_crime_reg.R
@@ -1,6 +1,16 @@
+### Load packages #########
 library(lmtest)
 library(sandwich)
 library(wooldridge)
+library(stargazer)
+###########################
+
+# Load data 
 data("crime1")
+
+# Run regression
 mymodel = lm(narr86 ~ inc86, data=crime1)
-coeftest(mymodel, vcov = vcovHC(mymodel, type="HC1"))
+# Make robust standard errors 
+robust_SEs = as.vector(coeftest(mymodel, vcov = vcovHC(mymodel, type="HC1"))[,"Std. Error"])
+# Show in table
+stargazer(mymodel,se=robust_SEs, type="text")

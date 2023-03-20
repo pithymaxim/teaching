d53ad97c385cfbe581943f6a2b48ff25dd6ed1cb
pithymaxim <maximplaysbass@gmail.com>
2023-03-20 07:58:58 -0700

    Update test_script.r

--- Rscraps/logging/test_script.r
+++ Rscraps/logging/test_script.r
@@ -1,2 +1,20 @@
-# Step 1: Some math:
+# Homework 1
+# March 20, 2023 
+# Jane Doe 
+  
+#########################################################
+# Question 1: Math                             ##########
+#########################################################
+
 1+2
+
+# I find that the answer is 3. 
+
+########################################################
+# Question 2: Load data and run regression    ##########
+########################################################
+
+data(mtcars)
+summary(lm(mpg~disp, data=mtcars))
+
+# The coefficient is -0.04.

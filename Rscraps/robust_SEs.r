aa66f2d8a5cf3daef391dcbe52e3c832d41ceb43
pithymaxim <maximplaysbass@gmail.com>
2023-03-26 19:04:05 -0700

    Update robust_SEs.r

--- Rscraps/robust_SEs.r
+++ Rscraps/robust_SEs.r
@@ -1,5 +1,11 @@
 ### Robust SEs
 library(lmtest)
 library(sandwich)
+
+# Using coeftest()
 mymodel = lm(bwght ~ male, data=bwght)
 coeftest(mymodel, vcov = vcovHC(mymodel, type="HC1"))
+
+# Using lm_robust()
+library(estimatr)
+lm_robust(mpg ~ disp, data=mtcars, se_type="HC1")

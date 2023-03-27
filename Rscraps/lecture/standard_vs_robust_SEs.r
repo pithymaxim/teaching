eff4f3a0f783389c378c7115ca38eef638ef3fef
pithymaxim <maximplaysbass@gmail.com>
2023-03-26 19:01:30 -0700

    Update standard_vs_robust_SEs.r

--- Rscraps/lecture/standard_vs_robust_SEs.r
+++ Rscraps/lecture/standard_vs_robust_SEs.r
@@ -6,10 +6,9 @@ auto <- read.dta("http://www.stata-press.com/data/r9/auto.dta")
 # Estimate model
 model = lm(price ~ mpg, data = auto)
 
-# Pull the default standard errors and calculate the robust standard errors
-se_standard = as.vector(summary(model)$coefficients[,"Std. Error"])
+# Calculate the robust standard errors
 se_robust  = as.vector(coeftest(model,vcov = vcovCL, type="HC1")[,"Std. Error"])
 
 # Show both in a table
 stargazer(model, model, type = "text",
-          se = list(se_standard,se_robust))
+          se = list(NULL,se_robust))

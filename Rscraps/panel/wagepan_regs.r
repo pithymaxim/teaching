d6648a5bbb626bcdb8ff1c02ca7eb1827eb0f3e3
pithymaxim <maximplaysbass@gmail.com>
2023-04-07 20:30:30 -0700

    Update wagepan_regs.r
    added clustered SEs and double demeaning

--- Rscraps/panel/wagepan_regs.r
+++ Rscraps/panel/wagepan_regs.r
@@ -3,9 +3,11 @@ library(lubridate)
 library(collapse)
 library(lfe)
 
+############################## Preliminaries
+### Load data 
 data("wagepan")
 
-#### Merge in CPI
+#### Merge in CPI and deflate wages 
 # Read in CPI data 
 cpi_df = read.csv("https://github.com/pithymaxim/teaching/raw/main/misc/cpi.csv")
 
@@ -15,11 +17,11 @@ wagepan = merge(wagepan,cpi_df,by="year")
 # Make variable with deflated wages 
 wagepan$def_wage = wagepan$lwage - log(wagepan$cpi)
 
-### Estimate regression
-# Simple bivariate regression 
+############################## Regressions  
+######## Simple bivariate regression 
 stargazer(lm(def_wage ~ married, data=wagepan), type="text", digits=5)
 
-# Adding person fixed effects (uses lfe package)
+######## Adding person fixed effects (uses lfe package)
 # After the "~" is a four-part formula: 
 # (1) Covariates
 # (2) Factors to be projected out (i.e., fixed effects)
@@ -28,15 +30,33 @@ stargazer(lm(def_wage ~ married, data=wagepan), type="text", digits=5)
 # Source: https://rdrr.io/cran/lfe/man/felm.html
 stargazer(felm(def_wage ~ married |nr | 0 | 0, wagepan), type="text", digits=5)
 
-### Demeaning. Take group-level averages with "ave" function
+######## Demeaning. Take group-level averages with "ave" function
 wagepan$demeaned_wage =    wagepan$def_wage - ave(wagepan$def_wage, wagepan$nr)
 wagepan$demeaned_married = wagepan$married  - ave(wagepan$married, wagepan$nr)
 
 # Show demeaned regression
 stargazer(lm(demeaned_wage ~ demeaned_married, data=wagepan), digits=5, type="text")
 
-### Two way fixed effects 
+######## Two way fixed effects 
 stargazer(felm(def_wage ~ married |nr + year| 0 | 0, wagepan), digits=5, type="text")
 
+######## Two way deameaning:
+wagepan$demeaned2_wage =    wagepan$def_wage - ave(wagepan$def_wage, wagepan$year) -
+                                              ave(wagepan$def_wage, wagepan$nr)
+wagepan$demeaned2_married = wagepan$married  - ave(wagepan$married, wagepan$year) - 
+                                               ave(wagepan$married, wagepan$nr)
+
+# Show demeaned regression (coefs are same as two way fixed effects regression)
+stargazer(lm(demeaned2_wage ~ demeaned2_married, data=wagepan), digits=5, type="text")
+
+######## Interaction 
 # Interact with hisp (hisp gets dropped, that's OK!)
 stargazer(felm(def_wage ~ married*hisp |nr + year| 0 | 0, wagepan), digits=5, type="text")
+
+######## Clustering at the <nr> level
+mod_twfe           = felm(def_wage ~ married |nr + year| 0 | 0, wagepan)
+mod_twfe_clustered = felm(def_wage ~ married |nr + year| 0 | nr, wagepan)
+
+# See that the standard errors change but not the coefs
+stargazer(mod_twfe,mod_twfe_clustered,digits=5,type="text")
+

f2212963da65ecc76cb2e16186b2d429bf8ee814
pithymaxim <maximplaysbass@gmail.com>
2023-04-07 20:22:42 -0700

    Update wagepan_regs.r
    adding stargazer and changed CPI source

--- Rscraps/panel/wagepan_regs.r
+++ Rscraps/panel/wagepan_regs.r
@@ -2,28 +2,22 @@ library(wooldridge)
 library(lubridate)
 library(collapse)
 library(lfe)
+
 data("wagepan")
 
 #### Merge in CPI
-# Read in the data 
-cpi = read.csv("https://datahub.io/core/cpi-us/r/cpiai.csv")
-# https://stackoverflow.com/questions/36568070/extract-year-from-date
-
-# Make year variable (uses lubridate package)
-cpi$year = year(ymd(cpi$Date))
-
-# Collapse to yearly level (uses collapse package)
-annual_cpi <- collap(cpi, Index ~ year, FUN = list(fmean))
+# Read in CPI data 
+cpi_df = read.csv("https://github.com/pithymaxim/teaching/raw/main/misc/cpi.csv")
 
 # Merge with wagepan data on year
-wagepan = merge(wagepan,annual_cpi,by="year")
+wagepan = merge(wagepan,cpi_df,by="year")
 
 # Make variable with deflated wages 
-wagepan$def_wage = wagepan$lwage - log(wagepan$Index)
+wagepan$def_wage = wagepan$lwage - log(wagepan$cpi)
 
 ### Estimate regression
 # Simple bivariate regression 
-summary(lm(def_wage ~ married, data=wagepan))
+stargazer(lm(def_wage ~ married, data=wagepan), type="text", digits=5)
 
 # Adding person fixed effects (uses lfe package)
 # After the "~" is a four-part formula: 
@@ -31,24 +25,18 @@ summary(lm(def_wage ~ married, data=wagepan))
 # (2) Factors to be projected out (i.e., fixed effects)
 # (3) Instruments (set to 0 to not use)
 # (4) Level at which to cluster standard errors (set to 0 to not use)
-summary(felm(def_wage ~ married |nr | 0 | 0, wagepan), robust="T")
+# Source: https://rdrr.io/cran/lfe/man/felm.html
+stargazer(felm(def_wage ~ married |nr | 0 | 0, wagepan), type="text", digits=5)
 
 ### Demeaning. Take group-level averages with "ave" function
-wagepan$demeaned_wage = wagepan$def_wage - ave(wagepan$def_wage, wagepan$nr)
-wagepan$demeaned_married = wagepan$married - ave(wagepan$married, wagepan$nr)
+wagepan$demeaned_wage =    wagepan$def_wage - ave(wagepan$def_wage, wagepan$nr)
+wagepan$demeaned_married = wagepan$married  - ave(wagepan$married, wagepan$nr)
 
-# Showing the right number of decimals is annoying...
-# https://stackoverflow.com/questions/65341848/how-can-i-print-a-certain-number-of-decimals-digits-in-r-when-scipen-and-digits
-round(summary(lm(demeaned_wage ~ demeaned_married, data=wagepan))$coefficients, 4)
+# Show demeaned regression
+stargazer(lm(demeaned_wage ~ demeaned_married, data=wagepan), digits=5, type="text")
 
 ### Two way fixed effects 
-# What's with the "0 | 0" ? Explanation: 
-# The formula specification is a response variable followed by a four part formula. 
-# The first part consists of ordinary covariates, the second part consists of factors to be projected out. 
-# The third part is an IV-specification. The fourth part is a cluster specification for the standard errors. 
-# Source: https://rdrr.io/cran/lfe/man/felm.html
-summary(felm(def_wage ~ married |nr + year| 0 | 0, wagepan), robust="T")
+stargazer(felm(def_wage ~ married |nr + year| 0 | 0, wagepan), digits=5, type="text")
 
-# Interact with hisp (hisp gets dropped, that's OK)
-summary(felm(def_wage ~ married*hisp |nr + year| 0 | 0, wagepan), robust="T")
-
+# Interact with hisp (hisp gets dropped, that's OK!)
+stargazer(felm(def_wage ~ married*hisp |nr + year| 0 | 0, wagepan), digits=5, type="text")

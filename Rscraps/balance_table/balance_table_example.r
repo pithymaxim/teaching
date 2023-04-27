767fc6f6160711685a7c5b6c50cf7e228e319517
pithymaxim <maximplaysbass@gmail.com>
2023-04-27 23:07:19 +0200

    Update balance_table_example.r

--- Rscraps/balance_table/balance_table_example.r
+++ Rscraps/balance_table/balance_table_example.r
@@ -14,20 +14,22 @@ mtcars_labels = read.table(header=TRUE, text="
   vs    'Engine'
   am    'Transmission'
   gear  'Number of forward gears'
-  carb  'Number of carburetors'
-")
-
-my_test_args=crosstable_test_args()
-my_test_args$show_method = FALSE
+  carb  'Number of carburetors'")
 
 # Apply column labels to mtcars 
 mydata = mtcars  %>%  import_labels(mtcars_labels, name_from="name", label_from="label")
 
+# Prepare arguments for crosstable
+my_test_args=crosstable_test_args()
+my_test_args$show_method = FALSE
+
+# Run crosstable
 my_table = crosstable(mydata,by=vs, test=TRUE, 
                       funs=c(mean=mean, "std error"=sd),
                       test_args=my_test_args) %>% 
   as_flextable()
 
+# Export as HTML
 save_as_html(my_table, path = "test_table.html")
 browseURL("test_table.html")
 

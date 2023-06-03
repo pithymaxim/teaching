2db0fc38ad9a32f213e5c8341a92fd1c2ae3c566
pithymaxim <maximplaysbass@gmail.com>
2023-06-03 07:04:08 -0700

    Update categorical.r

--- Rscraps/categorical.r
+++ Rscraps/categorical.r
@@ -4,3 +4,7 @@ summary(lm(kids ~ factor(relig), data=affairs))
 
 # Show means
 aggregate(affairs$kids, list(affairs$relig), mean)
+
+library(car)
+model = lm(kids ~ factor(relig), data=affairs)
+linearHypothesis(model, "factor(relig)2 = factor(relig)5")

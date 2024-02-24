641c279723be66516a6f7a617fe4aaaa467a8809
pithymaxim <maximplaysbass@gmail.com>
2024-02-24 10:58:25 -0800

    Update fwl_demo.r

--- Rscraps/fwl_demo.r
+++ Rscraps/fwl_demo.r
@@ -14,3 +14,30 @@ bwght$bwght_vs_faminc_residual <- resid(bwght_vs_faminc)
 stargazer(lm(bwght_vs_faminc_residual ~ cigs_vs_faminc_residual, data = bwght), type="text")
 stargazer(lm(bwght ~ cigs + faminc, data = bwght), type="text")
 
+# With categorical control:
+
+library(haven)
+library(dplyr)
+library(broom)
+df = read_dta("http://www.stata-press.com/data/r9/auto.dta")
+
+# --- (1)
+# Regress price on mpg, and then price on mpg and foreign
+summary(lm(price ~ mpg, data = df))
+summary(lm(price ~ mpg + foreign, data = df))
+
+# --- (2) Demeaning. Take group-level averages with "ave" function
+df$price_demeaned = df$price   - ave(df$price, df$foreign)
+df$mpg_demeaned   = df$mpg - ave(df$mpg, df$foreign)
+
+# Take the residual from regressing price on foreign
+df$price_resid_vs_foreign <- residuals(lm(price ~ foreign, data = df))
+
+# Show that price_demeaned and price_resid_vs_foreign are identical
+summary(lm(price_demeaned ~ price_resid_vs_foreign, data = df))
+
+# --- (3) 
+# Regression using demeaned variables
+lm1 = lm(price_demeaned ~ mpg_demeaned, data = df)
+lm2 = lm(price ~ mpg + foreign, data = df)
+stargazer(lm1,lm2,type="text")
